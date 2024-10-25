035165df050e219b5c5e63edd027bd160e0229a6
asim <asimbaidya.me@gmail.com>
2024-10-26 02:56:07 +0600

    backend: (routes) fix routes are added to main router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import test
-from app.api.routes import user
-from app.api.routes import login
-from app.api.routes import quiz
-from app.api.routes import course
-from app.api.routes import question
+from app.api.routes import test, user, login, student, teacher, admin
 
 
 api_router = APIRouter()
@@ -13,6 +8,8 @@ api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(test.router, prefix="/test", tags=["test"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
-api_router.include_router(quiz.router, prefix="/quiz", tags=["quiz"])
-api_router.include_router(course.router, prefix="/course", tags=["course"])
-api_router.include_router(question.router, prefix="/question", tags=["question"])
+
+# Role Based Routes
+api_router.include_router(student.router, prefix="/student", tags=["student"])
+api_router.include_router(teacher.router, prefix="/teacher", tags=["teacher"])
+api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
