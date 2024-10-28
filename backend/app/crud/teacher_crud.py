db81ba1cbece5f614f22851f5308a15bfd5163f2
asim <asimbaidya.me@gmail.com>
2024-10-29 02:15:22 +0600

    backend: (crud) crud function are elegant now with join to make it slower

--- backend/app/crud/teacher_crud.py
+++ backend/app/crud/teacher_crud.py
@@ -1,107 +1,176 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
-from app.models.quiz import Course, Question, Quiz
+from app.models.quiz import (
+    Course,
+    Enrollment,
+    Question,
+    QuestionSet,
+    QuestionSubmission,
+    Quiz,
+)
+from app.models.user import User
 from app.schemas.common import CourseCreate, QuizCreate
 from app.schemas.question import QuestionTeacherView
-
-
-# Refactor-start
-def create_course(db: Session, course_create: CourseCreate, creator_id: int) -> Course:
-    db_obj = Course(
-        title=course_create.title,
-        description=course_create.description,
-        creator_id=creator_id,
-        course_pin=course_create.course_pin,
-    )
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-
-    return db_obj
-
-
-def create_quiz(db: Session, quiz_create: QuizCreate, course_id: int) -> Quiz:
-    db_obj = Quiz(
-        course_id=course_id,
-        title=quiz_create.title,
-        total_mark=quiz_create.total_mark,
-    )
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-    return db_obj
-
-
-def create_question(
-    db: Session, question_create: QuestionTeacherView, quiz_id: int
-) -> Question:
-    db_obj = Question(
-        quiz_id=quiz_id,
-        # below here it's guaranteed to be a valid model after validation
-        question_data=question_create.question_data.model_dump(),  # type: ignore
-        tag=question_create.tag,
-    )
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-    return db_obj
-
-
-def get_course_creator_id(db: Session, course_id: int) -> int | None:
-    course = db.query(Course).filter(Course.id == course_id).first()
-    if course is None:
-        return None
-    return course.creator_id  # type: ignore
-
-
-def get_course_by_title(db: Session, course_title: str) -> Course | None:
-    return db.query(Course).filter(Course.title == course_title).first()
 
 
 def get_courses_by_creator_id(db: Session, creator_id: int) -> list[Course]:
     return db.query(Course).filter(Course.creator_id == creator_id).all()
 
 
-def get_quizzes_by_course_id(db: Session, course_id: int) -> list[Quiz]:
-    return db.query(Quiz).filter(Quiz.course_id == course_id).all()
+def get_quiz_by_course_title_creator_id(
+    db: Session, course_title: str, teacher_id: int
+):
+    return (
+        db.query(Quiz)
+        .join(Course)
+        .filter(Course.title == course_title, Course.creator_id == teacher_id)
+        .all()
+    )
 
 
-def get_quiz_by_id(db: Session, quiz_id: int) -> Quiz | None:
-    return db.query(Quiz).filter(Quiz.id == quiz_id).first()
+def get_enrolled_students(db: Session, course_title: str, teacher_id: int):
+    return (
+        db.query(User)
+        .join(Enrollment)
+        .join(Course)
+        .filter(course_title == course_title, User.id == teacher_id)
+        .all()
+    )
 
 
-def get_questions_by_quiz_id(db: Session, quiz_id: int) -> list[Question]:
-    return db.query(Question).filter(Question.quiz_id == quiz_id).all()
+def get_questions_by_course_title_quiz_id_teacher_id(
+    db: Session, course_title: str, quiz_id: int, teacher_id: int
+):
+    return (
+        db.query(Question)
+        .join(target=Quiz)
+        .join(Course)
+        .join(QuestionSet)
+        .filter(
+            Course.title == course_title,
+            Quiz.id == quiz_id,
+            Course.creator_id == teacher_id,
+        )
+        .all()
+    )
 
 
-def get_question_by_id(db: Session, question_id: int) -> Question | None:
-    return db.query(Question).filter(Question.id == question_id).first()
+def get_student_progress_course_title_quiz_id_teacher_id(
+    db: Session, course_title: str, quiz_id: int, teacher_id: int
+):
+    db.query(QuestionSubmission).join(Question).join(QuestionSet).join(Course).filter(
+        Course.title == course_title,
+        Quiz.id == quiz_id,
+        Course.creator_id == teacher_id,
+    ).all()
 
 
-def get_enrolled_students(db: Session, course_id: int):
-    return 'TODO'
+def create_course_by_teacher_id(
+    db: Session, course_create: CourseCreate, teacher_id: int
+) -> Course:
+    course_instance = Course(
+        title=course_create.title,
+        description=course_create.description,
+        creator_id=teacher_id,
+        course_pin=course_create.course_pin,
+    )
+    db.add(course_instance)
+    db.commit()
+    db.refresh(course_instance)
+    return course_instance
 
 
-def get_student_progress(db: Session, quiz_id: int):
-    return 'TODO'
-
-
-def check_course_owner(db: Session, course_title: str, teacher_id: int) -> Course:
-    """Check if the course exists and if the teacher is the owner."""
-    course = get_course_by_title(db, course_title)
+def create_quiz_by_course_title_teacher_id(
+    db: Session, quiz_create: QuizCreate, course_title: str, teacher_id: int
+) -> Quiz:
+    course = get_course_by_title_creator_id(db, course_title, teacher_id)
     if course is None:
         raise HTTPException(status_code=404, detail='Course not found')
-    if bool(course.creator_id != teacher_id):
+    if int(course.creator_id) != teacher_id:
         raise HTTPException(
-            status_code=403, detail='You are not authorized to access this course'
+            status_code=403,
+            detail='You are not authorized to create quiz in this course',
         )
-    return course
+
+    question_set = QuestionSet()
+    db.add(question_set)
+    db.commit()
+    db.refresh(question_set)
+
+    db_instance = Quiz(
+        course_id=course.id,
+        title=quiz_create.title,
+        total_mark=quiz_create.total_mark,
+        question_set_id=question_set.id,
+    )
+    db.add(db_instance)
+    db.commit()
+    db.refresh(db_instance)
+    return db_instance
 
 
-def check_quiz_owner(db: Session, quiz_id: int, course: Course) -> Quiz:
-    """Check if the quiz exists and belongs to the specified course."""
-    quiz = get_quiz_by_id(db, quiz_id)
-    if quiz is None or bool(quiz.course_id != course.id):
+def create_question_by_course_title_quiz_id_teacher_id(
+    db: Session,
+    question_create: QuestionTeacherView,
+    course_title: str,
+    quiz_id: int,
+    teacher_id: int,
+) -> Question:
+    course = get_course_by_title_creator_id(db, course_title, teacher_id)
+    if course is None:
+        raise HTTPException(status_code=404, detail='Course not found')
+    quiz = get_quiz_by_course_title_creator_id_quiz_id(
+        db, course_title, teacher_id, quiz_id
+    )
+    if quiz is None:
         raise HTTPException(status_code=404, detail='Quiz not found in this course')
-    return quiz
+
+    question_set = get_question_set_by_quiz_id(db, quiz_id)
+
+    question_instance = Question(
+        question_type=question_create.question_type,
+        question_data=question_create.question_data.model_dump(),
+        tag=question_create.tag,
+        question_set_id=question_set.id,
+    )
+
+    db.add(question_instance)
+    db.commit()
+    return question_instance
+
+
+# helper functions
+
+
+def get_quiz_by_course_title_creator_id_quiz_id(
+    db: Session, course_title: str, teacher_id: int, quiz_id: int
+):
+    return (
+        db.query(Quiz)
+        .join(Course)
+        .filter(
+            Course.title == course_title,
+            Course.creator_id == teacher_id,
+            Quiz.id == quiz_id,
+        )
+        .first()
+    )
+
+
+def get_question_set_by_quiz_id(db: Session, quiz_id: int):
+    question_set = db.query(QuestionSet).join(Quiz).filter(Quiz.id == quiz_id).first()
+    if question_set is None:
+        raise HTTPException(status_code=404, detail='Question set not found')
+    return question_set
+
+
+def get_course_by_title_creator_id(db: Session, course_title: str, teacher_id: int):
+    course = (
+        db.query(Course)
+        .filter(Course.title == course_title, Course.creator_id == teacher_id)
+        .first()
+    )
+    if course is None:
+        raise HTTPException(status_code=404, detail='Course not found')
+    return course
