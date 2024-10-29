9165500be14a579658ac35aa84efd2272a1570c6
asim <asimbaidya.me@gmail.com>
2024-10-29 10:19:31 +0600

    backend: initial data file with initial useers

--- backend/app/core/create_tables.py
+++ backend/app/core/create_tables.py
@@ -1,4 +1,5 @@
-from app.core.db import Base, engine
+from app.core.db import Base, engine, sessionmaker
+from app.core.initial_data import users
 
 # from app.models.note import Note
 from app.models.quiz import Quiz
@@ -6,11 +7,19 @@ from app.models.user import User
 
 __imported_tables = [User, Quiz]
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def create_all_tables():
     Base.metadata.create_all(engine)
     for table in Base.metadata.tables:
         print(f'{table} created')
+
+    for user in users:
+        user_instance = User(**user)
+        db = SessionLocal()
+        db.add(instance=user_instance)
+        db.commit()
 
 
 def drop_all_tables():
---
+++ backend/app/core/initial_data.py
@@ -0,0 +1,30 @@
+users = [
+    {
+        'full_name': 'John Doe',
+        'email': 'john_doe@gmail.com',
+        'hashed_password': '$2b$12$TOAxdt08NJfg4GQbFKSZyueKK7aExYvriA97dIkYEWh.IkOU4UCm6',
+        'role': 'teacher',
+        'joined_at': '2024-10-29 01:58:50.254303',
+    },
+    {
+        'full_name': 'John',
+        'email': 'john@gmail.com',
+        'hashed_password': '$2b$12$TOAxdt08NJfg4GQbFKSZyueKK7aExYvriA97dIkYEWh.IkOU4UCm6',
+        'role': 'teacher',
+        'joined_at': '2024-10-29 01:58:50.254303',
+    },
+    {
+        'full_name': 'Bil',
+        'email': 'bil@gmail.com',
+        'hashed_password': '$2b$12$TOAxdt08NJfg4GQbFKSZyueKK7aExYvriA97dIkYEWh.IkOU4UCm6',
+        'role': 'student',
+        'joined_at': '2024-10-29 01:58:50.254303',
+    },
+    {
+        'full_name': 'Bob',
+        'email': 'bob@gmail.com',
+        'hashed_password': '$2b$12$TOAxdt08NJfg4GQbFKSZyueKK7aExYvriA97dIkYEWh.IkOU4UCm6',
+        'role': 'student',
+        'joined_at': '2024-10-29 01:58:50.254303',
+    },
+]
